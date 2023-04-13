e8ab146fdc498be188db6646270f4d397d3795aa
yang <boyyang-love@outlook.com>
2023-04-13 17:40:14 +0800

    添加origin

--- common/origin/getOrigin.go
+++ common/origin/getOrigin.go
@@ -5,6 +5,7 @@ func GetOrigin() []string {
 		"http://localhost:3000",
 		"http://boyyanglove.web3v.vip",
 		"https://prod-2g5hif5wbec83baa-1301921121.tcloudbaseapp.com",
+		"http://boyyang.3vkj.club/",
 	}
 	return origin
 }
